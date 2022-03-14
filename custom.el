7a73590d72c7632fa6a05b5cd640e54e9da7ebe9
Daniel <steew0x8@protonmail.com>
2022-03-14 14:28:52 +0100

    Initial config

---
+++ custom.el
@@ -0,0 +1,26 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auth-sources '("~/.config/emacs/authinfo.gpg"))
+ '(browse-url-browser-function 'eww-browse-url)
+ '(custom-safe-themes
+   '("78e6be576f4a526d212d5f9a8798e5706990216e9be10174e3f3b015b8662e27" "46b2d7d5ab1ee639f81bde99fcd69eb6b53c09f7e54051a591288650c29135b0" "fb83a50c80de36f23aea5919e50e1bccd565ca5bb646af95729dc8c5f926cbf3" "e3a1b1fb50e3908e80514de38acbac74be2eb2777fc896e44b54ce44308e5330" "b02eae4d22362a941751f690032ea30c7c78d8ca8a1212fdae9eecad28a3587f" "b6269b0356ed8d9ed55b0dcea10b4e13227b89fd2af4452eee19ac88297b0f99" "24168c7e083ca0bbc87c68d3139ef39f072488703dcdd82343b8cab71c0f62a7" "c8b83e7692e77f3e2e46c08177b673da6e41b307805cd1982da9e2ea2e90e6d7" "c48551a5fb7b9fc019bf3f61ebf14cf7c9cdca79bcb2a4219195371c02268f11" "845489fb9f7547e6348a80f942402fc7ac7c6854b0accabc49aeddd8cd4a2bd9" "a9a67b318b7417adbedaab02f05fa679973e9718d9d26075c6235b1f0db703c8" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "3a9f65e0004068ecf4cf31f4e68ba49af56993c20258f3a49e06638c825fbfb6" default))
+ '(dired-sidebar-width 27)
+ '(display-line-numbers 'relative)
+ '(lsp-clangd-binary-path "/usr/local/bin/clangd13")
+ '(make-backup-files nil)
+ '(org-default-notes-file "~/.config/emacs/notes")
+ '(package-selected-packages
+   '(writeroom-mode doom-modeline monokai-theme monokai-alt-theme monokai-pro-theme helm-ag lsp seoul256-theme yasnippet-classic-snippets hl-todo yasnippet-snippets smartparens helm-c-yasnippet yasnippet helm-lsp rfc-mode helm-eww magit pinentry swiper-helm counsel lsp-ui lsp-mode helm expand-region company flycheck org-bullets dired-sidebar all-the-icons-dired dashboard use-package))
+ '(send-mail-function 'sendmail-send-it)
+ '(tab-width 4)
+ '(use-package-verbose 'debug)
+ '(w3m-search-default-engine "duckduckgo"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:background nil))))
